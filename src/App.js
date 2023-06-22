9ae219b7f32bf7062856749f91d48d8909129bbe
= <=>
2023-06-22 16:38:32 +0530

    Working On Time Travel Feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Square({value,onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board() {
+function Board() {
 
   const [xIsNext,setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -59,6 +59,20 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+// Adding Time Travel
+export default function Game(){
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */ }</ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares) {
